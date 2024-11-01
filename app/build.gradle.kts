2c45057645eb84c26eeef78edb8b43b5afcd501e
marcel <marcel.van.heerwaarden@gmail.com>
2024-11-01 09:01:51 +0100

    Update dependencies, making targetSdk 35 necessary.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2024. Marcel van Heerwaarden
+ *
+ * Copyright (C) 2019 The Android Open Source Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -7,12 +24,12 @@ plugins {
 
 android {
     namespace = "com.github.mheerwaarden.dynamictheme"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.mheerwaarden.dynamictheme"
         minSdk = 26 // Android 8.0 = Build.VERSION_CODES.O
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
